5066c4b2f7541c279d3c0f8a4717038df11756f2
amir <amir.nit97@gmail.com>
2024-09-29 14:50:39 +0330

    complete content_widget.dart and comment_widget.dart

--- lib/screens/profile/widgets/normal_user/comment_widget.dart
+++ lib/screens/profile/widgets/normal_user/comment_widget.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+
+import '../../../../configs/localization/app_localizations.dart';
+import '../../../../repos/models/comment.dart';
+import '../../../../repos/models/content.dart';
+import '../../profile_bloc.dart';
+import '../lists_items/comments.dart';
+import '../lists_items/contents.dart';
 
 class CommentWidget extends StatefulWidget {
   final int? profileId;
@@ -9,8 +17,61 @@ class CommentWidget extends StatefulWidget {
 }
 
 class _CommentWidgetState extends State<CommentWidget> {
+  final _pagingController0 = PagingController<int, Comment>(firstPageKey: 0);
+  @override
+  void initState() {
+    super.initState();
+    _pagingController0.addPageRequestListener((pageKey) {
+      ProfileBloc.fetchComment(_pagingController0, null, widget.profileId, "my", 20);
+    });
+  }
+  @override
+
+  dispose() {
+    _pagingController0.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: EdgeInsetsDirectional.only(top: 16),
+      child: Container(
+        padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Theme.of(context).colorScheme.background,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 8),
+            Text(AppLocalizations.of(context)!.translateNested("profileScreen", 'comments'),
+              style: Theme.of(context).textTheme.displayMedium!.copyWith(
+                color: Theme.of(context).primaryColor,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            SizedBox(height: 8),
+            Container(
+              height: 1,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Theme.of(context).primaryColor
+                        .withOpacity(0),
+                    Theme.of(context).primaryColor
+                        .withOpacity(1),
+                  ],
+                  stops: const [0.0, 1.0],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(child: Comments(pagingController: _pagingController0)),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/profile/widgets/normal_user/content_widget.dart
+++ lib/screens/profile/widgets/normal_user/content_widget.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+
+import '../../../../configs/localization/app_localizations.dart';
+import '../../../../repos/models/content.dart';
+import '../../profile_bloc.dart';
+import '../lists_items/contents.dart';
 
 class ContentWidget extends StatefulWidget {
   final int? profileId;
@@ -9,8 +15,61 @@ class ContentWidget extends StatefulWidget {
 }
 
 class _ContentWidgetState extends State<ContentWidget> {
+  final _pagingController0 = PagingController<int, Content>(firstPageKey: 0);
+  @override
+  void initState() {
+    super.initState();
+    _pagingController0.addPageRequestListener((pageKey) {
+      ProfileBloc.fetchContent(_pagingController0, 0, 20, widget.profileId);
+    });
+  }
+  @override
+
+  dispose() {
+    _pagingController0.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: EdgeInsetsDirectional.only(top: 16),
+      child: Container(
+        padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Theme.of(context).colorScheme.background,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 8),
+            Text(AppLocalizations.of(context)!.translateNested("bottomBar", 'content'),
+              style: Theme.of(context).textTheme.displayMedium!.copyWith(
+                color: Theme.of(context).primaryColor,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            SizedBox(height: 8),
+            Container(
+              height: 1,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Theme.of(context).primaryColor
+                        .withOpacity(0),
+                    Theme.of(context).primaryColor
+                        .withOpacity(1),
+                  ],
+                  stops: const [0.0, 1.0],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(child: Contents(pagingController: _pagingController0),),
+          ],
+        ),
+      ),
+    );
   }
 }
